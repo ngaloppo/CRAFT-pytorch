6ebcfe6413cc2b96bcc337d1a9dc1d5177f4f562
Nico Galoppo <nico.galoppo@intel.com>
2023-05-05 20:26:03 -0700

    OpenVINO export and testing

--- imgproc.py
+++ imgproc.py
@@ -13,7 +13,7 @@ def loadImage(img_file):
     if img.shape[0] == 2: img = img[0]
     if len(img.shape) == 2 : img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     if img.shape[2] == 4:   img = img[:,:,:3]
-    img = np.array(img)
+    img = np.array(img) # HWC
 
     return img
 
@@ -43,8 +43,8 @@ def resize_aspect_ratio(img, square_size, interpolation, mag_ratio=1):
     # set original image size
     if target_size > square_size:
         target_size = square_size
-    
-    ratio = target_size / max(height, width)    
+
+    ratio = target_size / max(height, width)
 
     target_h, target_w = int(height * ratio), int(width * ratio)
     proc = cv2.resize(img, (target_w, target_h), interpolation = interpolation)
